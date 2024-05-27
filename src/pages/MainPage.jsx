bd2ea36b57aa907e51648361df7da4834dd3f02e
kimbumsoo0820 <kbs2082@naver.com>
2024-05-27 11:28:57 +0900

    time

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,37 +1,11 @@
-import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 
 const Home = () => {
-  const [test,setTest] = useState([
-    {
-      name:'ㅎ',
-      age:15,
-    },
-    {
-      name:'z',
-      age:15,
-    },
-    {
-      name:'g',
-      age:15,
-    }
-  ])
 
-  const obj = {name:'kd',age:16}
-  // useEffect(()=> {
-  //   setTest([...test, obj])
-  // },[])
-  useEffect(()=> {
-    console.log(test)
-  },[test])
-  const testFunc = () => {
-    setTest([...test, obj])
-  }
   return (
     <>
       <div>Home</div>
       <Link to="/myPage">마이페이지로</Link>
-      <button onClick={()=> testFunc()}>추가</button>
     </>
   )
 }
---
+++ src/redux/reducers/userSlice.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState: [],
+    reducers: {
+        setUser: state => {
+            return [...state, ]
+        }
+})
