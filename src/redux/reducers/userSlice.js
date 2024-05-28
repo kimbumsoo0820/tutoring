578d13504acdd8d58fcd9c13ec403e19a7658a76
kimbumsoo0820 <kbs2082@naver.com>
2024-05-28 12:05:50 +0900

    [redux-toolkit]setting

--- src/redux/reducers/userSlice.js
+++ src/redux/reducers/userSlice.js
@@ -1,10 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = [
+  {
+    name: "홍길동",
+    age: 20,
+    address: "서울특별시",
+  },
+];
+
 const userSlice = createSlice({
-    name: 'user',
-    initialState: [],
-    reducers: {
-        setUser: state => {
-            return [...state, ]
-        }
-})
+  name: "users",
+  initialState: initialState,
+  reducers: {
+    addUser(state, action) {
+      state.push(action.payload);
+    },
+    setUserData(state, action) {
+      return action.payload;
+    },
+  },
+});
+
+export const { addUser, setUserData } = userSlice.actions;
+export default userSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./reducers/user.reducer";
+import userSlice from "./reducers/userSlice";
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: userSlice,
   },
 });
 
